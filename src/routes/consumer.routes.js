36021d9386eb0edf6b0cf1cbe0d5b9ecc4a3ecaa
feyishola <feyisholaologunebi@gmail.com>
2023-07-28 20:04:59 +0100

    modified the consumer routes by creating a rating route that sends a post request to the renders server

--- src/routes/consumer.routes.js
+++ src/routes/consumer.routes.js
@@ -1,134 +1,211 @@
-const consumerController = require('../controller/consumer.controller')
-const {Router} = require('express')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const microServiceConnector = require('../utils/nodefetch')
+const consumerController = require("../controller/consumer.controller");
+const { Router } = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const microServiceConnector = require("../utils/nodefetch");
 
-module.exports = ()=>{
-    const api = Router()
+module.exports = () => {
+  const api = Router();
 
-    api.get('/', async(req,res)=>{
-        try {
-            let response = await consumerController.getAllConsumers()
-            
-            res.status(200).json({response:true, payload:response})
-        } catch (error) {
-            res.status(500).json({response:false,payload:error.message})
+  api.get("/", async (req, res) => {
+    try {
+      let response = await consumerController.getAllConsumers();
+
+      res.status(200).json({ response: true, payload: response });
+    } catch (error) {
+      res.status(500).json({ response: false, payload: error.message });
+    }
+  });
+
+  api.get("/:id", async (req, res) => {
+    try {
+      let id = req.params.id;
+      let response = await consumerController.getSingleConsumer(id);
+      res.status(200).json({ response: true, payload: response });
+    } catch (error) {
+      res.status(500).json({ response: false, payload: error.message });
+    }
+  });
+
+  api.post("/", async (req, res) => {
+    try {
+      const {
+        consumerName,
+        phoneNumber,
+        locationUpdate,
+        permanentAddress,
+        temporaryAddress,
+        password,
+      } = req.body;
+
+      const saltRounds = 10;
+      const newPassword = await bcrypt.hash(password, saltRounds);
+
+      let response = await consumerController.createConsumer(
+        consumerName,
+        phoneNumber,
+        locationUpdate,
+        permanentAddress,
+        temporaryAddress,
+        newPassword
+      );
+      res.status(200).json({ response: true, payload: response });
+    } catch (error) {
+      res.status(500).json({ response: false, payload: error.message });
+    }
+  });
+
+  api.post("/login", async (req, res) => {
+    try {
+      const { phoneNumber, password } = req.body;
+
+      if (phoneNumber && password) {
+        const user = await consumerController.getUser(phoneNumber);
+
+        if (user !== null) {
+          let checkPassword = await bcrypt.compare(password, user.password);
+
+          if (checkPassword) {
+            let token = jwt.sign(
+              { userName: user.userName, phoneNumber, userType: user.userType },
+              process.env.SECRET_KEY,
+              { expiresIn: "1h" }
+            );
+
+            res.status(200).json({ response: true, payload: { user, token } });
+          } else {
+            res
+              .status(400)
+              .json({ response: false, payload: "Password is incorrect" });
+          }
+        } else {
+          res
+            .status(400)
+            .json({ response: false, payload: "Phone Number is incorrect" });
         }
-        
-    });
+      } else {
+        res
+          .status(400)
+          .json({ response: false, payload: "Both inputs are required" });
+      }
+    } catch (error) {
+      res.status(500).json({ response: false, payload: error.message });
+    }
+  });
 
-    
+  api.post("/search", async (req, res) => {
+    try {
+      const { service, lng, lat, meters } = req.body;
 
-    api.get('/:id',async(req,res)=>{
-        try {
-            let id = req.params.id
-            let response = await consumerController.getSingleConsumer(id)
-            res.status(200).json({response:true, payload:response})
-        } catch (error) {
-            res.status(500).json({response:false,payload:error.message})
-        }
-    })
+      let body = {
+        service,
+        lng,
+        lat,
+        meters,
+      };
 
-    api.post('/', async(req,res)=>{
-        try {
-            const {consumerName,phoneNumber,locationUpdate,permanentAddress,temporaryAddress,password} = req.body
+      const services = await microServiceConnector(
+        "http://localhost:5000/api/v1/services/search",
+        "POST",
+        body
+      );
 
-            const saltRounds = 10
-            const newPassword = await bcrypt.hash(password,saltRounds)
-            
-            let response = await consumerController.createConsumer(consumerName,phoneNumber,locationUpdate,permanentAddress,temporaryAddress,newPassword)
-            res.status(200).json({response:true, payload:response})
-        } catch (error) {
-            res.status(500).json({response:false,payload:error.message})
-        }
-    })
+      console.log({ "gotten by consumer": services });
 
-    api.post('/login',async(req,res)=>{
-        try {
-            const {phoneNumber,password} = req.body
+      if (services.response == true) {
+        res.status(200).json({ response: true, payload: services.payload });
+      } else {
+        throw new error("error occurred getting services");
+      }
+    } catch (error) {
+      res.status(500).json({ response: false, payload: error.message });
+    }
+  });
 
-            if(phoneNumber && password){
+  api.post("/ratings", async (req, res) => {
+    try {
+      const {
+        phoneNumber,
+        timeliness,
+        communication,
+        valueForMoney,
+        customerService,
+        professionalism,
+      } = req.body;
 
-                const user = await consumerController.getUser(phoneNumber)
-                
-                if(user!==null){
+      let body = {
+        phoneNumber,
+        timeliness,
+        communication,
+        valueForMoney,
+        customerService,
+        professionalism,
+      };
 
-                    let checkPassword = await bcrypt.compare(password,user.password)
-                    
-                    if(checkPassword){
-                        let token = jwt.sign({userName:user.userName,phoneNumber,userType:user.userType},process.env.SECRET_KEY,{expiresIn:"1h"})
+      const ratingResponse = await microServiceConnector(
+        "http://localhost:5000/api/v1/services/ratings",
+        "PATCH",
+        body
+      );
 
-                        res.status(200).json({response:true, payload:{user,token}})
+      if (ratingResponse.response == true) {
+        res
+          .status(200)
+          .json({ response: true, payload: "Thank you for your ratings" });
+      } else {
+        throw new error("error occurred rating");
+      }
+    } catch (error) {
+      res.status(500).json({ response: false, payload: error.message });
+    }
+  });
 
-                    }else{
-                        res.status(400).json({response:false, payload:"Password is incorrect"})
-                    }
-                }else{
-                    res.status(400).json({response:false, payload:"Phone Number is incorrect"})
-                }
-               
-            }else{
-                res.status(400).json({response:false,payload:"Both inputs are required"})
-            }
-        } catch (error) {
-            res.status(500).json({response:false,payload:error.message})
-        }
-    })
+  api.put("/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
 
-    api.post('/search', async(req,res)=>{
-        try {
-            const {service, lng, lat, meters} = req.body
+      const {
+        consumerName,
+        phoneNumber,
+        locationUpdate,
+        permanentAddress,
+        temporaryAddress,
+        userType,
+        password,
+        services,
+      } = req.body;
 
-            let body = {
-                service,
-                lng,
-                lat,
-                meters
-            }
+      const saltRounds = 10;
+      const newPassword = await bcrypt.hash(password, saltRounds);
+      let response = await consumerController.updateConsumer(
+        id,
+        consumerName,
+        phoneNumber,
+        locationUpdate,
+        permanentAddress,
+        temporaryAddress,
+        userType,
+        newPassword,
+        services
+      );
 
-            const services = await microServiceConnector('http://localhost:5000/api/v1/services/search',"POST",body)
+      res
+        .status(200)
+        .json({ response: true, payload: "Consumer record Updated" });
+    } catch (error) {
+      res.status(500).json({ response: false, payload: error.message });
+    }
+  });
 
-            console.log({"gotten by consumer":services});
+  api.delete("/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+      const response = await consumerController.deleteConsumer(id);
+      res.status(200).json({ response: true, payload: response });
+    } catch (error) {
+      res.status(500).json({ response: false, payload: error.message });
+    }
+  });
 
-            if(services.response == true){
-                
-                res.status(200).json({response:true, payload:services.payload})
-            }else{
-                throw new error("error occurred getting services")
-            }
-        } catch (error) {
-            res.status(500).json({response:false,payload:error.message})
-        }
-    })
-
-    api.put('/:id', async(req,res)=>{
-        try {
-            const id = req.params.id
-
-            const {consumerName,phoneNumber,locationUpdate,permanentAddress,temporaryAddress,userType,password,services} = req.body
-            
-            const saltRounds = 10
-            const newPassword = await bcrypt.hash(password,saltRounds)
-            let response = await consumerController.updateConsumer(id,consumerName,phoneNumber,locationUpdate,permanentAddress,temporaryAddress,userType,newPassword,services)
-
-            res.status(200).json({response:true, payload:"Consumer record Updated"})
-
-        } catch (error) {
-            res.status(500).json({response:false,payload:error.message})
-        }
-    })
-
-    api.delete('/:id',async(req,res)=>{
-        try {
-            const id = req.params.id
-            const response = await consumerController.deleteConsumer(id)
-            res.status(200).json({response:true, payload:response})
-        } catch (error) {
-            res.status(500).json({response:false, payload:error.message})
-        }
-       
-    })
-
-    return api;
-}
+  return api;
+};
